66cbc4ce90c5b2f3091bd733a4fc692459d09fb3
MpMp <mpmp@mpmp-desktop.(none)>
2013-01-26 13:39:02 +0100

    Added enum TimeInMilliseconds and TimeInSeconds.

---
+++ Core/DateTime/TimeInMilliseconds.php
@@ -0,0 +1,12 @@
+<?php
+namespace MToolkit\Core\DateTime;
+
+final class TimeInMilliseconds
+{
+    const MILLISECOND=1;
+    const SECOND=1000;
+    const MINUTE=60000;
+    const HOUR=3600000;
+    const DAY=86400000;
+    const WEEK=604800000;
+}
---
+++ Core/DateTime/TimeInSeconds.php
@@ -0,0 +1,12 @@
+<?php
+namespace MToolkit\Core\DateTime;
+
+final class TimeInSeconds
+{
+    const MILLISECOND=0.001;
+    const SECOND=1;
+    const MINUTE=60;
+    const HOUR=3600;
+    const DAY=86400;
+    const WEEK=604800;
+}
