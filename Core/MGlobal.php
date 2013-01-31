1705f7257566eba8fc011435b6b6a264e0dfb71c
Michele Pagnin <michelepagnin@ppambianchi.khmed.local>
2013-01-31 18:04:47 +0100

    Improved in the core.

--- Core/MGlobal.php
+++ Core/MGlobal.php
@@ -18,5 +18,22 @@
  * @author  Michele Pagnin
  */
 
-require_once dirname(__FILE__) . '/MGlobal.php';
-require_once dirname(__FILE__) . '/MObject.php';
+/**
+ * Prints a warning message containing the source code file name and line number if <i>$test</i> is false.
+ * M_ASSERT() is useful for testing pre- and post-conditions during development. 
+ * 
+ * @param boolean $test
+ */
+function M_ASSERT($test)
+{
+    if ($test !== true)
+    {
+        $trace = debug_backtrace();
+        $lastTrace = $trace[0];
+
+        $output = 'ASSERT FAIL: "%b" in file %s, line %s.<br />';
+        $output = sprintf($output, $lastTrace['args'][0], $lastTrace['file'], $lastTrace['line']);
+
+        trigger_error($output, E_USER_WARNING);
+    }
+}
--- Core/MString.php
+++ Core/MString.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace MToolkit\Core;
 
 /*
@@ -20,46 +21,50 @@ namespace MToolkit\Core;
  * @author  Michele Pagnin
  */
 
-require_once dirname(__FILE__).'/Enum/CaseSensitivity.php';
-require_once dirname(__FILE__).'/Exception/MWrongTypeException.php';
+require_once dirname(__FILE__) . '/Enum/CaseSensitivity.php';
+require_once dirname(__FILE__) . '/Exception/MWrongTypeException.php';
+
+use MToolkit\Core\Enum\CaseSensitivity;
+use MToolkit\Core\Exception\MWrongTypeException;
 
 /**
  * The MString class provides a php string. 
  */
 class MString
 {
-    private $text="";
-    
-    public function __construct( $text="" )
+
+    /**
+     * @var string
+     */
+    private $text = "";
+
+    /**
+     * 
+     * @param MString|string $text
+     * @throws MWrongTypeException
+     */
+    public function __construct($text = "")
     {
-        if( ($text instanceof MString)===false && is_string($text)===false )
-        {
-            throw new MWrongTypeException( "\$text", "MString or string", gettype($text) );
-        }
-        
-        $this->text=(string)$text;
+        $this->text = (string) $text;
     }
-    
+
     /**
      * Appends the string <i>$text</i> onto the end of this string.
      * @param MString|string $text
      * @throws WrongTypeException 
      */
-    public function append( $text )
+    public function append($str)
     {
-        if( ($text instanceof MString)===false && is_string($text)===false )
-        {
-            throw new MWrongTypeException( "\$text", "MString or string", gettype($text) );
-        }
-        
-        $this->text=(string)$text;
+        $string=(string)$str;
+
+        return new MString( $this->text.$string );
     }
-    
+
     public function __toString()
     {
-        return (string)$this->text;
+        return (string) $this->text;
     }
-    
+
     /**
      * Returns the character at the given <i>$i</i> position in the string.
      * The position must be a valid index position in the string (i.e., 0 <= position < size()).
@@ -67,80 +72,81 @@ class MString
      * @return string|null
      * @throws WrongTypeException 
      */
-    public function at( $i )
+    public function at($i)
     {
-        $result=substr($this->text, $i, 1);
-        
-        if( $result===false )
+        $result = substr($this->text, $i, 1);
+
+        if ($result === false)
         {
             return null;
         }
-        
+
         return $result;
     }
-    
+
     /**
      * Removes <i>$n</i> characters from the end of the string.
      * If <i>$n</i> is greater than size(), the result is an empty string.
+     * 
      * @param int $n
      * @throws WrongTypeException 
      */
-    public function chop( $n )
+    public function chop($n)
     {
-        if( $n>=$this->size() )
+        if ($n >= $this->size())
         {
-            $this->text="";
+            $this->text = "";
             return;
         }
-        
-        $result=substr($this->text, $i, strlen($this->text)-$n);
-        
-        if( $result===true )
+
+        $result = substr($this->text, $i, strlen($this->text) - $n);
+
+        if ($result === true)
         {
-            $this->text=$result;
+            $this->text = $result;
         }
     }
-    
+
     /**
      * Clears the contents of the string and makes it empty. 
      */
     public function clear()
     {
-        $this->text="";
+        $this->text = "";
     }
-    
+
     /**
      * Compares this string with <i>$other</i> and returns an integer less than, 
      * equal to, or greater than zero if this string is less than, equal to, or greater than <i>$other</i>.
      * If <i>$cs</i> is Qt::CaseSensitive, the comparison is case sensitive; 
      * otherwise the comparison is case insensitive.
      * 
-     * @param MString $other
-     * @param type $cs
-     * @return type 
+     * @param MString|string $other
+     * @param CaseSensitivity $cs
+     * @return int 
      */
-    public function /* int */ compare( MString $other, $cs=CaseSensitivity::CaseSensitive )
+    public function compare($other, $cs = CaseSensitivity::CaseSensitive)
     {
-        $text=(string)$this->text;
-        $o=(string)$other;
-        
-        switch( $cs )
+        $text = (string) $this->text;
+        $o = (string) $other;
+
+        switch ($cs)
         {
             case CaseSensitivity::CaseSensitive:
-                
+
                 break;
             case CaseSensitivity::CaseInsensitive:
-                $o=strtolower( $other );
-                $text=strtolower( $this->text );
+                $o = strtolower($other);
+                $text = strtolower($this->text);
                 break;
             default:
-                throw new MWrongTypeException( "\$cs", "CaseSensitivity", gettype($cs) );
+                throw new MWrongTypeException("\$cs", "CaseSensitivity", gettype($cs));
                 break;
         }
-        
-        return strcmp( $text, $o );
+
+        return strcmp($text, $o);
     }
-    
+
     /**
      * Returns true if this string contains an occurrence of the string <i>$str</i>; otherwise returns false.
      * 
@@ -148,48 +154,248 @@ class MString
      * @param CaseSensitivity $cs
      * @return type 
      */
-    public function /* bool */ contains( MString $str, $cs = CaseSensitivity::CaseSensitive )
+    public function /* bool */ contains(MString $str, $cs = CaseSensitivity::CaseSensitive)
     {
-        $text=(string)$this->text;
-        $s=(string)$str;
-        
-        switch( $cs )
+        $text = (string) $this->text;
+        $s = (string) $str;
+
+        switch ($cs)
         {
             case CaseSensitivity::CaseSensitive:
-                
+
                 break;
             case CaseSensitivity::CaseInsensitive:
-                $s=strtolower( $other );
-                $text=strtolower( $this->text );
+                $s = strtolower($other);
+                $text = strtolower($this->text);
                 break;
             default:
-                throw new MWrongTypeException( "\$cs", "CaseSensitivity", gettype($cs) );
+                throw new MWrongTypeException("\$cs", "CaseSensitivity", gettype($cs));
                 break;
         }
-        
-        $result=  strpos($text, $s);
-        
-        return ( $result!==false );
+
+        $result = strpos($text, $s);
+
+        return ( $result !== false );
     }
-    
+
     /**
      * Returns the number of characters in this string.
      * The last character in the string is at position size() - 1. 
      * 
      * @return int 
      */
-    public /* int */ function size()
+    public function size()
     {
         return strlen($this->text);
     }
-    
+
+    public function count()
+    {
+        return $this->size();
+    }
+
     /**
-     * @param string $string
+     * Returns the index position of the first occurrence of the string str in this string, 
+     * searching forward from index position from. Returns -1 if str is not found.
+     * 
+     * @param string|MString $str
+     * @param int $from
+     * @param CaseSensitivity $cs
+     * @return int 
+     */
+    public function indexOf($str, $from = 0, $cs = CaseSensitivity::CaseInsensitive)
+    {
+        $toSearch = (string) $str;
+        $pos = false;
+
+        switch ($cs)
+        {
+            case CaseSensitivity::CaseInsensitive:
+                $pos = stripos($this->text, $toSearch, $from);
+                break;
+            case CaseSensitivity::CaseSensitive:
+                $pos = strpos($this->text, $toSearch, $from);
+                break;
+        }
+
+        if ($pos === false)
+        {
+            return -1;
+        }
+
+        return $pos;
+    }
+
+    /**
+     * Returns the index position of the last occurrence of the string str in this string, 
+     * searching backward from index position from. If from is -1 (default), 
+     * the search starts at the last character; if from is -2, at the next to last character and so on. Returns -1 if str is not found.
+     * 
+     * @param string|MString $str
+     * @param int $from
+     * @param CaseSensitivity $cs
+     * @return int
+     */
+    public function lastIndexOf($str, $from = 0, $cs = CaseSensitivity::CaseInsensitive)
+    {
+        $toSearch = (string) $str;
+        $pos = false;
+
+        switch ($cs)
+        {
+            case CaseSensitivity::CaseInsensitive:
+                $pos = strripos($this->text, $toSearch, $from);
+                break;
+            case CaseSensitivity::CaseSensitive:
+                $pos = strrpos($this->text, $toSearch, $from);
+                break;
+        }
+
+        if ($pos === false)
+        {
+            return -1;
+        }
+
+        return $pos;
+    }
+
+    /**
+     * Inserts the string str at the given index position and returns a reference to this string.
+     * 
+     * @param int $position
+     * @param string|MString $str
+     * @return MString
+     */
+    public function insert($position, $str)
+    {
+        $string = (string) $str;
+
+        $string = substr_replace($this->text, $string, $position, 0);
+
+        return new MString($string);
+    }
+
+    /**
+     * Returns a substring that contains the n leftmost characters of the string.
+     * The entire string is returned if n is greater than size() or less than zero.
+     * 
+     * @param int $n
+     * @return null|MString
+     */
+    public function left($n)
+    {
+        if ($n < 0 || $n > $this->size())
+        {
+            return new MString($this->text);
+        }
+
+        $result = substr($this->text, 0, $n);
+
+        if ($result === false)
+        {
+            return null;
+        }
+
+        return new MString($result);
+    }
+
+    /**
+     * Returns a substring that contains the n rightmost characters of the string.
+     * The entire string is returned if n is greater than size() or less than zero.
+     * 
+     * @param int $n
+     * @return MString|null
+     */
+    public function right($n)
+    {
+        if ($n < 0 || $n > $this->size())
+        {
+            return new MString($this->text);
+        }
+
+        $pos = $n * -1;
+        $result = substr($this->text, $pos);
+        
+        if ($result === false)
+        {
+            return null;
+        }
+
+        return new MString($result);
+    }
+
+    /**
+     * @param string|MString $string
      * @return bool
      */
-    public static function isNullOrEmpty( $string )
+    public static function isNullOrEmpty($string)
     {
-        return ( $string==null || $string=='' );
+        $str = (string) $string;
+
+        return ( $str == null || $str == '' );
+    }
+    
+    /**
+     * Returns true if this string is null; otherwise returns false.
+     * 
+     * @return boolean
+     */
+    public function isNull()
+    {
+        return ( $this->text==null );
+    }
+
+    /**
+     * Returns true if the string has no characters; otherwise returns false.
+     * 
+     * @return boolean
+     */
+    public function isEmpty()
+    {
+        return ( $this->text=='' );
+    }
+    
+    /**
+     * Returns a string that contains <i>$n</i> characters of this string, 
+     * starting at the specified <i>$position</i> index.<br />
+     * <br />
+     * Returns a null string if the position index exceeds the length of the string. 
+     * If there are less than n characters available in the string starting at the given position, 
+     * or if n is -1 (default), the function returns all characters that are available from the specified position.
+     * 
+     * @param int $position
+     * @param int $n
+     * @return null|MString
+     */
+    public function mid($position, $n = -1)
+    {
+        $str=null;
+        
+        if( $n==-1 )
+        {
+            $str=substr($this->text, $position);
+        }
+        else
+        {
+            $str=substr($this->text, $position, $n);
+        }
+        
+        return new MString($str);
+    }
+    
+    /**
+     * Prepends the string <i>$str</i> to the beginning of this string and returns a reference to this string.
+     * 
+     * @param string|MString $str
+     * @return MString
+     */
+    public function prepend( $str)
+    {
+        $string=(string)$str;
+        
+        $string=$string.$this->text;
+        
+        return new MString( $string );
     }
 }
 
