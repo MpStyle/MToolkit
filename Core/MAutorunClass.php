673fad258a979ecf9c2aa3b29fe0057b3c2600a8
MpMp <agv46@tiscali.it>
2013-09-21 13:49:48 +0200

    Create MAutorunClass: you can extend this class and implement the only one abstract method (run()) to create an autorunning class. The run() is the entry point method.

---
+++ Core/MAutorunClass.php
@@ -0,0 +1,53 @@
+<?php
+namespace MToolkit\Core;
+
+/*
+ * This file is part of MToolkit.
+ *
+ * MToolkit is free software: you can redistribute it and/or modify
+ * it under the terms of the LGNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MToolkit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * LGNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the LGNU Lesser General Public License
+ * along with MToolkit.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @author  Michele Pagnin
+ */
+
+require_once 'MObject.php';
+
+use MToolkit\Core\MObject;
+
+abstract class MAutorunClass extends MObject 
+{
+    public abstract function run();
+    
+    public static function autorun()
+    {
+        /* @var $classes string[] */ $classes = get_declared_classes();
+
+        /* @var $entryPoint string */ $entryPoint = $classes[count( $classes ) - 1];
+
+        /* @var $controller \MToolkit\Controller\MAbstractController */ $controller = new $entryPoint();
+
+        if ( ( $controller instanceof \MToolkit\Core\MAutorunClass ) === false )
+        {
+            $message = sprintf( "Invalid object for entry point in class %s, it must be an instance of MAutorunClass, %s is passed.", get_class( $this ), get_class( $controller ) );
+
+            throw new \Exception( $message );
+        }
+        
+        $controller->run();
+
+        // Clean the $_SESSION from signals.
+        $controller->disconnectSignals();
+    }
+}
+
+register_shutdown_function(array('\MToolkit\Core\MAutorunClass','autorun'));
