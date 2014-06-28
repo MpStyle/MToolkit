4a03843fa2f4b080c4d1fd1bfce157557bc9506f
Michele Pagnin <agv46@tiscali.it>
2014-06-28 14:03:58 +0200

    PHPDocumentation

--- Core/MPost.php
+++ Core/MPost.php
@@ -26,6 +26,9 @@ require_once __DIR__.'/Exception/MReadOnlyObjectException.php';
 use MToolkit\Core\MMap;
 use MToolkit\Core\Exception\MReadOnlyObjectException;
 
+/**
+ * Allows the access to the post parameters.
+ */
 class MPost extends MMap
 {
     public function __construct( )
